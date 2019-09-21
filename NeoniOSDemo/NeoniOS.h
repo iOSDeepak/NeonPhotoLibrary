4731220acc312a878c3944f9e2fa4f8414522151
DEEPAK SHARMA <deepak.sharma1@girnarsoft.com>
2019-09-21 22:41:43 +0530

    Initial commit

---
+++ NeoniOSDemo/NeoniOS.h
@@ -0,0 +1,19 @@
+//
+//  NeoniOS.h
+//  NeoniOS
+//
+//  Created by Deepak Sharma on 17/09/19.
+//  Copyright © 2019 GirnarSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NeoniOS.
+FOUNDATION_EXPORT double NeoniOSVersionNumber;
+
+//! Project version string for NeoniOS.
+FOUNDATION_EXPORT const unsigned char NeoniOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeoniOS/PublicHeader.h>
+
+
---
+++ NeoniOSDemo/NeoniOSDemo.h
@@ -0,0 +1,19 @@
+//
+//  NeoniOSDemo.h
+//  NeoniOSDemo
+//
+//  Created by Deepak Sharma on 20/09/19.
+//  Copyright © 2019 GirnarSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NeoniOSDemo.
+FOUNDATION_EXPORT double NeoniOSDemoVersionNumber;
+
+//! Project version string for NeoniOSDemo.
+FOUNDATION_EXPORT const unsigned char NeoniOSDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeoniOSDemo/PublicHeader.h>
+
+
