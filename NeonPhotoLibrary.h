6b139cff33d9250e34991367d31ef93e1fd86b76
DEEPAK SHARMA <deepak.sharma1@girnarsoft.com>
2019-09-25 00:58:15 +0530

    Change file structure.

---
+++ NeonPhotoLibrary.h
@@ -0,0 +1,13 @@
+//
+//  NeonPhotoLibrary.h
+//  NeoniOSDemo
+//
+//  Created by Deepak Sharma on 25/09/19.
+//  Copyright © 2019 GirnarSoft. All rights reserved.
+//
+
+#ifndef NeonPhotoLibrary_h
+#define NeonPhotoLibrary_h
+
+
+#endif /* NeonPhotoLibrary_h */
